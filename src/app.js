db99cac8e578a5343ad311e1505dbe2e25ac9757
Sahasranshu <sahasranshubehera@gmail.com>
2025-12-30 16:57:40 +0530

    added register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from express
-import cors from cors
+import express from "express"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express();
@@ -11,6 +11,16 @@ app.use(cors({
 
 //configuration
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoed({extended:true,limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes import 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -2,30 +2,11 @@
 import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
-
+import app from "./app.js";
 dotenv.config();
 
-const app = express()
 
 
-//=> Two Ways of coo=nnectiing to the mongo db  first is doing it in the index file that is here only 
-
-// ( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error",(error)=>{
-//             console.log("ERROR:",error)
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT,() => {
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-
-//     } catch (error) {
-//         console.log("ERROR:", error)
-//     }
-// }) 
 
 // Second way
 
