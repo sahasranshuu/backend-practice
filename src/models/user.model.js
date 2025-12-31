dbb5b825321506f92bbead02694f2a0b9b64498d
Sahasranshu <sahasranshubehera@gmail.com>
2026-01-01 01:10:28 +0530

    add loginUser and logoutUser functionalities

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = function(){
       },
       process.env.ACCESS_TOKEN_SECRET,
       {
-        expiresIn: ACCESS_TOKEN_EXPIRY
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
       }
     );
 }
@@ -82,7 +82,7 @@ userSchema.methods.generateRefreshToken = function () {
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: REFRESH_TOKEN_EXPIRY,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
